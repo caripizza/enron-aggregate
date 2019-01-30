0fb404de7d9454f3f06f065cb6e8d9f6aa23d440
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-29 17:41:43 -0800

    built out aggregate stages and tests pass!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const enron = require('./lib/connect');
 
 enron()
--- test/connect.js
+++ test/connect.js
@@ -3,16 +3,16 @@ const { MongoClient } = require('mongodb');
 let close = null;
 let collection = null;
 beforeAll(() => {
-    return MongoClient.connect('mongodb://alchemy:s3cret@ds249233.mlab.com:49233/enron', { useNewUrlParser: true })
-        .then(client => {
-            collection = client.db('enron').collection('enron_messages');
-            close = client.close.bind(client);
-            return collection;
-        });
+  return MongoClient.connect('mongodb://alchemy:s3cret@ds249233.mlab.com:49233/enron', { useNewUrlParser: true })
+    .then(client => {
+      collection = client.db('enron').collection('enron_messages');
+      close = client.close.bind(client);
+      return collection;
+    });
 });
 
 afterAll(() => {
-    return close(true);
+  return close(true);
 });
 
 
